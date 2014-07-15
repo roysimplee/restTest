f4828daf46e67bf838a53e8ecaaf75f9abd3d28c
Roy <roy@simplee.com>
2014-07-15 12:39:20 +0300

    init the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestManager::Application.config.secret_token = '773c6568d0137bb6052c6f9dd7c50393c6737fd99fcb9fbd4567d9f81f15d2e170f996e2bdb8cc2d166dd36b5c436fb29c8d716c02f554bf0ab2271f7a11c3d2'
